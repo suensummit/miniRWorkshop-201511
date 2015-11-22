6eb38f408e83b6754bd50364403fe5a6ec88986b
Summit Suen <summit.suen@gmail.com>
2015-11-22 20:31:07 +0800

    update live coding

--- live20151122.R
+++ live20151122.R
@@ -1,1 +1,29 @@
 # live coding
+rm(a)
+rm(list = c("pkgs", "t"))
+
+?ls
+ls()
+rm(list = ls())
+
+# install.packages("Lahman")
+?require
+?"library"
+library(Lahman)
+
+head(Batting)
+View(Batting)
+
+mean(Batting$AB)
+mean(Batting$AB, na.rm = TRUE)
+
+library(dplyr)
+aa <- cbind(AB = Batting$AB, yearID = Batting$yearID)
+bb <- select(Batting, AB, yearID)
+View(aa)
+View(bb)
+
+cc <- filter(bb, yearID > 2000)
+View(cc)
+
+?summary
